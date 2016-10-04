6941114ea57311da8fa6da2115034a882de43cea
Wachalski Pierre <pierre.wachalski@etu.utc.fr>
2016-10-04 18:02:08 +0200

    Petit script pour tester la génération de données

---
+++ testRunScript.py
@@ -0,0 +1,11 @@
+import matplotlib.pylab as plt
+import numpy as np
+
+from classes.SampleGenerator.MultimodalGenerator import MultimodalGenerator
+
+sample = MultimodalGenerator([(100,-1,1),(400,5,2)]).generateNormalSamples()
+hist, bins = np.histogram(sample, bins=50)
+width = 0.7 * (bins[1] - bins[0])
+center = (bins[:-1] + bins[1:]) / 2
+plt.bar(center, hist, align='center', width=width)
+plt.show()
