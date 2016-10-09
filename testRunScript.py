923786ff6d64662d65c66294c58bb334e461915f
Guillaume Dendievel <guillaume.dendievel@gmail.com>
2016-10-09 17:36:43 +0200

    ajout d'un commentaire pour la correspondance frc-eng

--- testRunScript.py
+++ testRunScript.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from classes.SampleGenerator.MultimodalGenerator import MultimodalGenerator
 
-sample = MultimodalGenerator([(100,-1,1),(400,5,2)]).generateNormalSamples()
+sample = MultimodalGenerator([(100,-1,1),(400,5,2)]).generateNormalSamples()    #sample = echantillon
 hist, bins = np.histogram(sample, bins=50)
 width = 0.7 * (bins[1] - bins[0])
 center = (bins[:-1] + bins[1:]) / 2
