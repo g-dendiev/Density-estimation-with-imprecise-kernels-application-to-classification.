7cb8e5a092ec402fc1660d4e9f31620d2ac74fb7
Guillaume Dendievel <guillaume.dendievel@gmail.com>
2016-10-09 20:51:13 +0200

    Changement pour le parametre de init qui n'est plus initialisé dans cette partie du programme

--- classes/Kernels/TriangularKernel.py
+++ classes/Kernels/TriangularKernel.py
@@ -6,12 +6,12 @@ class TriangularKernel(Kernel):
     """
 
     # Constructeur de classe
-    def __init__(self, h = 1):
+    def __init__(self, h):
         super().__init__(h)     #super() -> pour appeler le parent
         self._name = "Kernel Triangulaire"
 
     def value(self, centerPoint, seekedPoint):
-        if abs(centerPoint - seekedPoint) < (self.bandwidth / 2):
+        if abs(centerPoint - seekedPoint) <= (self.bandwidth / 2):
             # Thales !!!!
             return 2/self._bandwidth - 4*abs(centerPoint - seekedPoint)/(self._bandwidth*self.bandwidth)
         else:
