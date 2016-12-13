6012c8f2bd5dab8bc1926265634092434191216f
Guillaume Dendievel <guillaume.dendievel@gmail.com>
2016-12-13 11:15:09 +0100

    Adaptation du script pour appliquer la théorie sur des multimodale et mettre ça dans un csv :)

--- Test/testTriKernelChgtEpsilon.py
+++ Test/testTriKernelChgtEpsilon.py
@@ -24,13 +24,13 @@ dataset=[]
 
 with open('resultEpsilon.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['nbPoints'] + ['hOpt'] + ['epsilon'] + ['hInfTri'] + ['hSupTri'] + ['fHInf'] + ['fHSup']+ ['ecartRalatif'] + ['rapport en % de Epsilon sur hOpt'])
+    spamwriter.writerow(['nbPoints'] + ['hOpt'] + ['epsilon'] + ['hInfTri'] + ['hSupTri'] + ['fHInf'] + ['fHSup']+ ['ecartRalatif'] +  ['aireHOpt'] + ['rapport en % de Epsilon sur hOpt'])
 
 
 
 
 """Boucle sur le nombre de points :"""
-for nbPoints in my_range2(10,1000,2):    #Partir avec peu de points (10) et aller jusqu'à 1000. Doubler le nombre à chaque fois.
+for nbPoints in my_range2(1,1050,2):    #Partir avec peu de points (10) et aller jusqu'à 1000. Doubler le nombre à chaque fois.
 
     """Def du domaine d'étude, on a ici nbPoints de 10 à 1000 points entre -4 et 12"""
     defDomain = np.linspace(-4, 12, nbPoints)
@@ -73,61 +73,12 @@ for nbPoints in my_range2(10,1000,2):    #Partir avec peu de points (10) et alle
             hSupTri = hSupTriDict.get('potentialHValue')
             fHSup=hSupTriDict.get('maxedValue')
 
-            # for x in defDomain:
-            #
-            #     """remise à zéro de nos variables pour remplir les tableaux de valeurs de f(hMin) et f(hMax)"""
-            #     fx = 0
-            #     gx = 0
-            #     hx = 0
-            #
-            #
-            #
-            #
-            #     """Ici on passe aux Kernel avec un h minimal pour le point x dans le domaine de definition"""
-            #
-            #     tKernelTri = TriangularKernel(hInfTri)
-            #
-            #
-            #     for j in sample:
-            #         gx += tKernelTri.value(x,j)
-            #     yTriHMinOnDomain.append(gx)
-            #
-            #
-            #     """Ici on passe aux Kernel avec un h maximal pour le point x dans le domaine de definition"""
-            #
-            #     tKernelTri = TriangularKernel(hSupTri)
-            #
-            #     for j in sample:
-            #         hx += tKernelTri.value(x, j)
-            #     yTriHMaxOnDomain.append(hx)
-            #
-            # """plt.figure(figsize=(10,8))"""
-            #
-            # """Histogramme pour voir la tête de la répartition"""
-            # """hist, bins = np.histogram(sample, bins=15)
-            # width = 0.7 * (bins[1] - bins[0])
-            # center = (bins[:-1] + bins[1:]) / 2
-            # plt.subplot(223)
-            # barlist = plt.bar(center, hist, align='center', width=width)
-            # for bar in barlist:
-            #     bar.set_color('y')"""
-            #
-            # """ plt.plot(defDomain, yTriHMinOnDomain, label="TriHMin")
-            # plt.plot(defDomain, yTriHMaxOnDomain, label="TriHMax")
-            # plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-            # plt.gca().set_position([0, 0, 0.8, 0.8])
-            # plt.show()"""
-            #
-            # aireHMax = deri.trapz(yTriHMaxOnDomain)
-            # aireHMin = deri.trapz(yTriHMinOnDomain)
-            # aireExacte = deri.trapz(sample)
-            #
-            # ecartExacEtMin = (abs(aireHMin-aireExacte)/aireExacte)*100
-            # ecartExacEtMax = (abs(aireHMax-aireExacte)/aireExacte)*100
-            #
+            aireExacte = deri.trapz(sample)
+
+
             ecartRalatif = ((abs(fHSup-fHInf))/fHInf)*100
             rapportEpsHOpt = epsilon/hOpt*100
 
             with open('resultEpsilon.csv', 'a') as csvfile:
                 spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow([nbPoints] + [hOpt] + [epsilon] + [hInfTri] + [hSupTri] + [fHInf] + [fHSup]+ [ecartRalatif] + [rapportEpsHOpt])
+                spamwriter.writerow([nbPoints] + [hOpt] + [epsilon] + [hInfTri] + [hSupTri] + [fHInf] + [fHSup]+ [ecartRalatif] +  [aireExacte] + [rapportEpsHOpt])
