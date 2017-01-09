02853bbd528301e2df58eead307e7df77b7fdc8d
Guillaume Dendievel <guillaume.dendievel@gmail.com>
2017-01-09 14:34:36 +0100

    il faut voir ce que l'on peut faire mais le tKernel.domain a un nombre de valeurs qui varie et du coup on a un problème quand on veut plot car on a pas les mêmes dimensions !

--- Test/testRunTriangularKernelScript.py
+++ Test/testRunTriangularKernelScript.py
@@ -43,10 +43,12 @@ yTriHOptOnDomain = []
 yTriHMaxOnDomain = []
 yTriHMinOnDomain = []
 
-
+etape=0
 
 for pt in tKernelTri.domain:
     # Def des structures qui vont récolter les données (dans la boucle pour une remise à 0 à chaque cycle
+    etape+=1
+    print("etape",etape,"len de domain",len(tKernelTri.domain))
 
     structHOpt = {
         'potentialHValue': -1,
@@ -74,9 +76,35 @@ for pt in tKernelTri.domain:
     yTriHMinOnDomain.append(structHMin['minValue'])
 
 
-plt.plot(tKernelTri.domain, yTriHOptOnDomain, label="TriHOpt")
-plt.plot(tKernelTri.domain, yTriHMaxOnDomain, label="TriHMax")
-plt.plot(tKernelTri.domain, yTriHMinOnDomain, label="TriHMin")
+print("hOpt tableau ma gueule")
+print(yTriHOptOnDomain)
+
+print("taille hOpt tableau ma gueule")
+print(len(yTriHOptOnDomain))
+
+print("hMax tableau ma gueule")
+print(yTriHMaxOnDomain)
+
+print("taille hOpt tableau ma gueule")
+print(len(yTriHMaxOnDomain))
+
+print("hMin tableau ma gueule")
+print(yTriHMinOnDomain)
+
+print("taille hOpt tableau ma gueule")
+print(len(yTriHMinOnDomain))
+
+x=np.array(tKernelTri.domain)
+y1=np.array(yTriHOptOnDomain)
+y2=np.array(yTriHMaxOnDomain)
+y3=np.array(yTriHMinOnDomain)
+
+print("du love",x)
+print(len(x))
+
+plt.plot(x, y1, label="TriHOpt")
+plt.plot(x, y2, label="TriHMax")
+plt.plot(x, y3, label="TriHMin")
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 plt.gca().set_position([0, 0, 0.8, 0.8])
 plt.show()
